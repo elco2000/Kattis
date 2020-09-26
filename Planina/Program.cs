69ba60f6fe7d8d47418a6879b8998ae906cf2f28
elco2000 <elcomussert@gmail.com>
2020-09-26 18:18:38 +0200

    Planina Solution

---
+++ Planina/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Planina
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int s = 2;
+
+            int g = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                s += g;
+                g += g;
+            }
+
+            Console.WriteLine(s*s);
+        }
+    }
+}
