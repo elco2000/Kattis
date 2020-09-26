740ad04dda2737d04c5e2ebb8ad70988bbd68c58
elco2000 <elcomussert@gmail.com>
2020-09-26 17:03:51 +0200

    QALY Solution

---
+++ QALY/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace QALY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            double total = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                string[] NLines = Console.ReadLine().Split(" ");
+                total += double.Parse(NLines[0]) * double.Parse(NLines[1]);
+            }
+
+            Console.WriteLine(total);
+        }
+    }
+}
