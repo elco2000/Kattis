130e735bd757ec69ac1e026c1c5773a4b119fad6
elco2000 <elcomussert@gmail.com>
2020-09-26 17:18:40 +0200

    Pot Solution

---
+++ Pot/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Pot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int Total = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                int p = int.Parse(Console.ReadLine());
+                Total += (int) Math.Pow(p / 10, p % 10);
+            }
+
+            Console.WriteLine(Total);
+        }
+    }
+}
